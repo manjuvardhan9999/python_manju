00022ab123690fa49036a52507d6db9893a8c204
manjuvardhan9999 <manjuvardhanvaddi@gmail.com>
2025-10-27 20:00:16 +0530

    Time: 23 ms (20.46%), Space: 12.5 MB (7.19%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -4,20 +4,21 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
+        INT_MAX = 2**31 - 1  # 2147483647
+        INT_MIN = -2**31     # -2147483648
+
+        rev = 0
         sign = -1 if x < 0 else 1
         x = abs(x)
-        
-        rev = 0
-        while x != 0:
-            digit = x % 10
-            x //= 10
-            rev = rev * 10 + digit
-        
-        rev *= sign
-        
-        # 32-bit signed integer range check
-        if rev < -2**31 or rev > 2**31 - 1:
-            return 0
-        
-        return rev
 
+        while x != 0:
+            pop = x % 10
+            x //= 10
+
+            # Check for overflow before adding the next digit
+            if rev > (INT_MAX - pop) // 10:
+                return 0
+
+            rev = rev * 10 + pop
+
+        return sign * rev
